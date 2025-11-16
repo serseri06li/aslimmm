73de5a70ea48ee8f97f432ff0a9c593459845294
serseri06li <kaganmurat123@gmail.com>
2025-11-16 21:51:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const questionContainer = document.querySelector(".question-container");
+const resultContainer = document.querySelector(".result-container");
+const gifResult = document.querySelector(".gif-result");
+const heartLoader = document.querySelector(".cssload-main");
+const yesBtn = document.querySelector(".js-yes-btn");
+const noBtn = document.querySelector(".js-no-btn");
+
+// /change the postion of no button
+noBtn.addEventListener("mouseover", () => {
+  const newX = Math.floor(Math.random() * questionContainer.offsetWidth);
+  const newY = Math.floor(Math.random() * questionContainer.offsetWidth);
+
+  noBtn.style.left = `${newX}px`;
+  noBtn.style.top = `${newY}px`;
+});
+
+// yes button functionality
+
+yesBtn.addEventListener("click", () => {
+  // Navigate to the Valentine's Day page
+  window.location.href = "file:///C:/Users/bilim/Downloads/çıkma%20teklifi-20251116T173038Z-1-001/çıkma%20teklifi/index.html";
+});
